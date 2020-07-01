05689c55fdc38eced3f5594b6f2ab0b9f33c9b76
script000kiddies000 <scriptkiddies@protonmail.com>
2020-07-02 01:41:04 +0700

    template blade laravel

---
+++ resources/views/templates/adminlte/inc/sidebar-custom.blade.php
@@ -0,0 +1,48 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Brand Logo -->
+  <a href="../../index3.html" class="brand-link">
+    <img src="{{ asset('/templates/adminlte3') }}/dist/img/AdminLTELogo.png"
+         alt="AdminLTE Logo"
+         class="brand-image img-circle elevation-3"
+         style="opacity: .8">
+    <span class="brand-text font-weight-light">AdminLTE 3</span>
+  </a>
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+        <img src="{{ asset('/templates/adminlte3') }}/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+      </div>
+      <div class="info">
+        <a href="#" class="d-block">Alexander Pierce</a>
+      </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-item">
+          <a href="{{ route('normal-table') }}" class="nav-link">
+            <i class="nav-icon fas fa-table"></i>
+            <p>
+              Simple Table
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="{{ route('datatables') }}" class="nav-link">
+            <i class="nav-icon fas fa-table"></i>
+            <p>
+              Datatables
+            </p>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
